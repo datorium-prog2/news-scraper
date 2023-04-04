5270536532091bf2ffa0baa7e52b2571384a98ae
Serzsh <serzants.janis@gmail.com>
2023-04-04 14:04:37 +0300

    Added cheerio adn axios and made a sample

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const cheerio = require("cheerio")
+const axios = require("axios")
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+async function performDelfiScraping() {
+    // downloading the target web page
+    // by performing an HTTP GET request in Axios
+    const axiosResponse = await axios.request({
+        method: "GET",
+        url: "https://www.delfi.lv/",
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
+    })
+
+    return cheerio.load(axiosResponse.data)
+}
+
+
+app.get('/', async (req, res) => {
+    const pageData = await performDelfiScraping()
+
+    console.log(pageData("body"))
+
   res.send('Hello World!')
 })
 

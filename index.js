62b76d33ae8b14d51db34e36c8ce2d63ed2caf7b
Serzsh <serzants.janis@gmail.com>
2023-04-11 13:41:23 +0300

    Updates

--- index.js
+++ index.js
@@ -57,24 +57,70 @@ async function performTVnetScraping() {
         headingText: headingText,
         articleUrl: articleUrl,
     }
-  }
+}
 
+async function performLsmScraping() {
+    // downloading the target web page
+    // by performing an HTTP GET request in Axios
+    const axiosResponse = await axios.request({
+        method: "GET",
+        url: "https://www.lsm.lv/",
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
+    })
+
+    const $ = cheerio.load(axiosResponse.data)
+    const image = $("article:first-of-type img")
+    const imageSrc = image.attr("data-src")
+
+    const link = $("article:first-of-type a")
+    const linkSrc = link.attr("href")
+
+    const heading = $("article:first-of-type figcaption span").first()
+    const headingText = heading.text().trim()
+
+    return {
+        imageSrc: imageSrc,
+        headingText: headingText,
+        linkSrc: linkSrc,
+    }
+}
 
 app.get('/', async (req, res) => {
-    const delfiData = await performDelfiScraping()
-    const tvnetData = await performTVnetScraping()
+    const [delfiData, tvnetData, lsmData] = await Promise.all([
+        performDelfiScraping(), 
+        performTVnetScraping(), 
+        performLsmScraping()
+    ])
 
     res.send(`
-        <a href="${delfiData.articleUrl}" target="_blank">
-            <img src="${delfiData.imageSrc}" alt="${delfiData.headingText}" width="400">
-            <h2>${delfiData.headingText}</h2>    
-        </a>
-        <a href="${tvnetData.articleUrl}" target="_blank">
-            <img src="${tvnetData.imageSrc}" alt="${tvnetData.headingText}" width="400">
-            <h2>${tvnetData.headingText}</h2>    
-        </a>
+        <div style="display: flex; gap: 10px;">
+            <div>
+                <h1>Populārākā delfi ziņa</h1>
+                <a href="${delfiData.articleUrl}" target="_blank">
+                    <img src="${delfiData.imageSrc}" alt="${delfiData.headingText}" width="400">
+                    <h2>${delfiData.headingText}</h2>    
+                </a>
+            </div>
+            <div>
+                <h1>Populārākā tvnet ziņa</h1>
+                <a href="${tvnetData.articleUrl}" target="_blank">
+                    <img src="${tvnetData.imageSrc}" alt="${tvnetData.headingText}" width="400">
+                    <h2>${tvnetData.headingText}</h2>    
+                </a>
+            </div>
+            <div>
+                <h1>Populārākā lsm ziņa</h1>
+                <a href="${lsmData.articleUrl}" target="_blank">
+                    <img src="${lsmData.imageSrc}" alt="${lsmData.headingText}" width="400">
+                    <h2>${lsmData.headingText}</h2>    
+                </a>
+            </div>
+        </div>
     `)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

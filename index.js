b4a0b190efaf8c041a1803aad0d37f5bdf0d3337
Serzsh <serzants.janis@gmail.com>
2023-04-11 15:14:06 +0300

    updates

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const axios = require("axios")
 const app = express()
 const port = 3000
 
-async function performDelfiScraping() {
+const getDataFromUrl = async (url) => {
     // downloading the target web page
     // by performing an HTTP GET request in Axios
     const axiosResponse = await axios.request({
         method: "GET",
-        url: "https://www.delfi.lv/",
+        url: url,
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
         }
     })
 
-    const $ = cheerio.load(axiosResponse.data)
+    return cheerio.load(axiosResponse.data)
+}
+
+
+async function performDelfiScraping() {
+    const $ = await getDataFromUrl("https://www.delfi.lv/")
 
     const image = $('article:first-of-type img').first()
     const heading = $('article:first-of-type .headline__title').first()
@@ -32,18 +37,8 @@ async function performDelfiScraping() {
     }
 }
 
-async function performTVnetScraping() {
-    // downloading the target web page
-    // by performing an HTTP GET request in Axios
-    const axiosResponse = await axios.request({
-        method: "GET",
-        url: "https://www.tvnet.lv/",
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-        }
-    })
-  
-    const $ = cheerio.load(axiosResponse.data)
+async function performTVnetScraping() {  
+    const $ = await getDataFromUrl("https://www.tvnet.lv/")
   
     const image = $('.group-topic-with-custom-header article.list-article--1 div.list-article__image').first()
     const heading = $('.group-topic-with-custom-header article.list-article--1 div.list-article__headline').first()
@@ -60,17 +55,8 @@ async function performTVnetScraping() {
 }
 
 async function performLsmScraping() {
-    // downloading the target web page
-    // by performing an HTTP GET request in Axios
-    const axiosResponse = await axios.request({
-        method: "GET",
-        url: "https://www.lsm.lv/",
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-        }
-    })
+    const $ = await getDataFromUrl("https://www.lsm.lv/")
 
-    const $ = cheerio.load(axiosResponse.data)
     const image = $("article:first-of-type img")
     const imageSrc = image.attr("data-src")
 
@@ -87,20 +73,50 @@ async function performLsmScraping() {
     }
 }
 
+const performNamedaysScraping = async () => {
+    const $ = await getDataFromUrl("https://xn--kalendrs-m7a.lv/%C5%A1odien")
+
+    const nameArr = []
+
+    const primaryNames = $("ul.day-common li")
+    const additionalNames = $("p.day-common").text()
+
+    primaryNames.each((i, elm) => {
+        nameArr.push($(elm).text())
+    });
+
+    const additionalNamesArr = additionalNames.split(',')
+
+    return [
+        ...nameArr,
+        ...additionalNamesArr,
+    ]
+}
+
+
+const performBitcounScraping = async () => {
+    const $ = await getDataFromUrl("https://www.google.com/search?q=bitcoin+value&oq=bitcoin+value&aqs=chrome..69i57j0i512l9.2961j1j4&sourceid=chrome&ie=UTF-8")
+    const value = $('.pclqee').text()
+    
+    return `${value} €`
+}
+
+
 app.get('/', async (req, res) => {
-    const [delfiData, tvnetData, lsmData] = await Promise.all([
+    const [delfiData, tvnetData, lsmData, namesDayNames, bitcoinValue] = await Promise.all([
         performDelfiScraping(), 
         performTVnetScraping(), 
-        performLsmScraping()
+        performLsmScraping(),
+        performNamedaysScraping(),
+        performBitcounScraping(),
     ])
 
-    // no https://xn--kalendrs-m7a.lv/%C5%A1odien noskrāpēt šodienas vārda dienas jubilārus
-    // un ievietot viņus h5 tagā
     res.send(`
         <div>
-             <h5>
-                Šodien vārda dienu svin: <Parastos un paplašinātos vārdus>
-             </h5>
+            <h1>Vārda dienas</h1>
+             <h3>
+                ${namesDayNames.join(', ')}
+             </h3>
         </div>
         <div style="display: flex; gap: 10px;">
             <div>
@@ -125,6 +141,8 @@ app.get('/', async (req, res) => {
                 </a>
             </div>
         </div>
+        <h1>Bitcoin vērtība</h1>
+        <h2>${bitcoinValue}</h2>
     `)
 })
 
